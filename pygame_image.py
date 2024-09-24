736b0554f6e784c60c80add2a49dc5c1f06a6ef4
c0b23049 <c0b2304920@edu.teu.ac.jp>
2024-09-24 16:06:08 +0900

    練習7-1と7-2

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,21 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+    bg2_img = pg.transform.flip(bg_img,True,False)
+    kk_img = pg.transform.rotozoom(kk_img,10,1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=-(tmr%800)
+        x=-(tmr%3200)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg2_img, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
+        screen.blit(bg2_img, [x+4800, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(2000)
 
 
 if __name__ == "__main__":
